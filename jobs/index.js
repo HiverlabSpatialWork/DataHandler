74d3a7bcba0cb910c79379a121876ae9fd6e102c
George Chew <george@hiverlab.com>
2021-11-25 15:16:35 +0800

    feat: jobs now runs on start before intervals (run once on start, then x mins thereafter)

--- jobs/index.js
+++ jobs/index.js
@@ -3,15 +3,18 @@ const path = require('path');
 const jobs = [
     {
         name: path.join('fetch', 'loading-plan'),
-        interval: '5m'
+        interval: '5m',
+        timeout: 0
     },
     {
         name: path.join('fetch', 'inv-loc-complete'),
-        interval: '5m'
+        interval: '5m',
+        timeout: 0
     },
     {
         name: path.join('fetch', 'flight-schedule'),
-        interval: '5m'
+        interval: '5m',
+        timeout: 0
     }
 ]
 
