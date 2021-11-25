76eda50109bc77ffe51ad4956f8498b348a74618
George Chew <george@hiverlab.com>
2021-11-25 15:14:30 +0800

    fix: removed user, pw, and port from mongodb connection string

--- helper/config.js
+++ helper/config.js
@@ -1,11 +1,12 @@
 const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST = '127.0.0.1',
+    DB_PORT = '3000',
+    DB_NAME = 'data-handler',
 } = process.env;
 
 module.exports = {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    //url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    url: `mongodb://${DB_HOST}/${DB_NAME}`
 };
