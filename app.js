6845954fdc3b62656af6aa896bf742601fd3d025
Hirak Chhatbar <hirak@xcitech.in>
2022-01-04 22:51:06 +0530

    - Add dependency for disaster

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/*', index);
+app.use("/disaster", disaster);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
