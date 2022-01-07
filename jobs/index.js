749030ea7e6bb39406c24dbe24eb33ed0b76dcca
George Chew <george@hiverlab.com>
2022-01-07 22:20:34 +0800

    feat: updated jobs intervals

--- jobs/index.js
+++ jobs/index.js
@@ -1,73 +1,64 @@
 const path = require('path');
 
 const jobs = [];
-//jobs.push({
-//    name: path.join('fetch', 'fetchBMSEnergy_Q1'),
-//    cron: '0 * * * *', // on start and then on minute 0 of every hour
-//    timeout: jobs.length * 10000
-//});
-//jobs.push({
-//    name: path.join('fetch', 'fetchBMSEnergy_Q2'),
-//    cron: '0 * * * *', // on start and then on minute 0 of every hour
-//    timeout: jobs.length * 10000
-//});
-//jobs.push({
-//    name: path.join('transform', 'bms_energy'),
-//    cron: '1 * * * *', // on start and then on minute 1 of every hour
-//    timeout: jobs.length * 10000
-//});
-//jobs.push({
-//    name: path.join('fetch', 'weather'),
-//    interval: '5m', // every 5 minutes
-//    timeout: jobs.length * 10000
-//});
-//jobs.push({
-//    name: path.join('fetch', 'fetchOrdersData'),
-//    interval: '10m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
-//jobs.push({
-//    name: path.join('fetch', 'fetchSKUMaster'),
-//    interval: '10m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
-//jobs.push({
-//    name: path.join('fetch', 'fetchOrderDetailData'),
-//    interval: '10m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
-//timeout: 0
-//jobs.push({
-//    name: path.join('fetch', 'fetchPickDetailData'),
-//    interval: '10m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
-//jobs.push({
-//    name: path.join('fetch', 'fetchAFCLShipments'),
-//    interval: '10m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
-//jobs.push({
-//    name: path.join('transform', 'outbound_orders'),
-//    interval: '5m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
-//jobs.push({
-//    name: path.join('transform', 'outbound_alerts'),
-//    interval: '5m', // every 5 minutes
-//    //timeout: jobs.length * 10000
-//    timeout: 0
-//});
+jobs.push({
+    name: path.join('fetch', 'fetchBMSEnergy_Q1'),
+    cron: '0 * * * *', // on start and then on minute 0 of every hour
+    timeout: 0 // run on start
+});
+jobs.push({
+    name: path.join('fetch', 'fetchBMSEnergy_Q2'),
+    cron: '0 * * * *',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('transform', 'bms_energy'),
+    cron: '1 * * * *',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('fetch', 'weather'),
+    interval: '60m', // every 60 minutes
+    timeout: 0
+});
+jobs.push({
+    name: path.join('fetch', 'fetchOrdersData'),
+    interval: '30m',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('fetch', 'fetchSKUMaster'),
+    interval: '30m',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('fetch', 'fetchOrderDetailData'),
+    interval: '30m',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('fetch', 'fetchPickDetailData'),
+    interval: '10m',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('fetch', 'fetchAFCLShipments'),
+    interval: '10m',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('transform', 'outbound_orders'),
+    interval: '10m',
+    timeout: 0
+});
+jobs.push({
+    name: path.join('transform', 'outbound_alerts'),
+    interval: '10m',
+    timeout: 0
+});
 jobs.push({
     name: path.join('transform', 'inbound_alerts'),
-    interval: '5m', // every 5 minutes
-    //timeout: jobs.length * 10000
+    interval: '10m',
     timeout: 0
 });
 
