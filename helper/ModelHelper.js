11f24469daeaac11933c774412fdc225555d389e
George Chew <george@hiverlab.com>
2022-03-29 14:48:07 +0800

    feat: removed all jobs except one to make 'empty' project

--- helper/ModelHelper.js
+++ helper/ModelHelper.js
@@ -1,42 +1,19 @@
-const modelPaths = {
-}
-
 // collections that store data in a 'sql-like' way
 const rawDataModels = {
-    fetchBMSEnergy_Q1: '../models/fetch/model-fetchBMSEnergy_Q1',
-    fetchBMSEnergy_Q2: '../models/fetch/model-fetchBMSEnergy_Q2',
-    fetchOrdersData: '../models/fetch/model-fetchOrdersData',
-    fetchOrderDetailData: '../models/fetch/model-fetchOrderDetailData',
-    fetchPickDetailData: '../models/fetch/model-fetchPickDetailData',
-    fetchSKUMaster: '../models/fetch/model-fetchSKUMaster',
-    fetchAFCLShipments: '../models/fetch/model-fetchAFCLShipments',
-    outbound_orders: '../models/transform/model-outbound_orders',
+    fetchBMSEnergy_Q1: '../jobs/bms_energy/model-fetchBMSEnergy_Q1',
+    fetchBMSEnergy_Q2: '../jobs/bms_energy/model-fetchBMSEnergy_Q2',
 }
 
 // collections that store data in 'snapshots', and has a timestamp for each entry
 const transformedDataModels = {
-    bms_energy: '../models/transform/model-bms_energy',
-    weather: '../models/fetch/model-weather.js',
-    outbound_alerts: '../models/transform/model-outbound_alerts',
-    inbound_alerts: '../models/transform/model-inbound_alerts',
-    flights: '../models/transform/model-flights.js',
+    bms_energy: '../jobs/bms_energy/model-bms_energy',
 }
 
 const models = {
     fetchBMSEnergy_Q1: require(rawDataModels.fetchBMSEnergy_Q1),
     fetchBMSEnergy_Q2: require(rawDataModels.fetchBMSEnergy_Q2),
-    fetchOrdersData: require(rawDataModels.fetchOrdersData),
-    fetchOrderDetailData: require(rawDataModels.fetchOrderDetailData),
-    fetchPickDetailData: require(rawDataModels.fetchPickDetailData),
-    fetchSKUMaster: require(rawDataModels.fetchSKUMaster),
-    fetchAFCLShipments: require(rawDataModels.fetchAFCLShipments),
-    outbound_orders: require(rawDataModels.outbound_orders),
 
     bms_energy: require(transformedDataModels.bms_energy),
-    weather: require(transformedDataModels.weather),
-    outbound_alerts: require(transformedDataModels.outbound_alerts),
-    inbound_alerts: require(transformedDataModels.inbound_alerts),
-    flights: require(transformedDataModels.flights),
 }
 
 function isRawData(modelName) {
